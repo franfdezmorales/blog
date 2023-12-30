fabfcd884d7052cd27743900850250a7e6bb8081
Francisco Fernández <franfdezmorales@gmail.com>
2023-12-30 16:07:38 +0100

    fix import error of discord provider

--- app/lib/auth/index.ts
+++ app/lib/auth/index.ts
@@ -2,6 +2,6 @@ import NextAuth from 'next-auth'
 import { type Provider } from 'next-auth/providers'
 import GitHub from '@auth/core/providers/github'
 import Google from '@auth/core/providers/google'
-import Discord from '@auth/core/providers/github'
+import Discord from '@auth/core/providers/discord'
 
 export const { handlers: { GET, POST }, auth } = NextAuth({ providers: [GitHub as Provider, Google as Provider, Discord as Provider] })
